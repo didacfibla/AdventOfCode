fccb362475384e3df69f93a12c40c8f92a5202e3
didac <hardware@eriaconsulting.com>
2023-12-21 11:55:19 +0100

    Day 5 without ranges

--- 2023/5/main.py
+++ 2023/5/main.py
@@ -1,106 +1,71 @@
-from itertools import zip_longest
+import re
+import sys
 
-seeds = []
-sed_to_soil = []
-soil_to_fertilizer = []
-fertilizer_to_water = []
-water_to_light = []
-light_to_temperature = []
-temperature_to_humidity = []
-humidity_to_location = []
-
-sed_to_soil_map = {}
-soil_to_fertilizer_map = {}
-fertilizer_to_water_map = {}
-water_to_light_map = {}
-light_to_temperature_map = {}
-temperature_to_humidity_map = {}
-humidity_to_location_map = {}
+pattern = re.compile("\\d+")
 
 
-def get_input_data_to_list():
-    lines = open("input", 'r').read()
-    lines = lines.replace('\n\n', '\n')
-    lines = lines.replace('\n', ' ')
-    lines = lines.replace(' map:', '')
-    lines = lines.replace('seeds: ', '')
-    lines = lines.split()
-
-    # convertirmos los numeros a int
-    for i in range(0, len(lines)):
-        if lines[i].isdigit():
-            lines[i] = int(lines[i])
-
-    # print(lines)
-
-    # Cargamos las semillas
-    i = 0
-    while isinstance(lines[i], (int, float)):
-        # Si el elemento es un número, haz algo con él
-        seeds.append(lines[i])
-        i += 1
-
-    print(f"Seeds: {seeds}")
-
-    i += 1
-    while isinstance(lines[i], (int, float)):
-        # Si el elemento es un número, haz algo con él
-        sed_to_soil.append(lines[i])
-        i += 1
-    print(f"sed_to_soil: {sed_to_soil}")
-
-    i += 1
-    while isinstance(lines[i], (int, float)):
-        # Si el elemento es un número, haz algo con él
-        soil_to_fertilizer.append(lines[i])
-        i += 1
-    print(f"soil_to_fertilizer: {soil_to_fertilizer}")
-
-    i += 1
-    while isinstance(lines[i], (int, float)):
-        # Si el elemento es un número, haz algo con él
-        fertilizer_to_water.append(lines[i])
-        i += 1
-    print(f"fertilizer_to_water: {fertilizer_to_water}")
-
-    i += 1
-    while isinstance(lines[i], (int, float)):
-        # Si el elemento es un número, haz algo con él
-        water_to_light.append(lines[i])
-        i += 1
-    print(f"water_to_light: {water_to_light}")
-
-    i += 1
-    while isinstance(lines[i], (int, float)):
-        # Si el elemento es un número, haz algo con él
-        light_to_temperature.append(lines[i])
-        i += 1
-    print(f"light_to_temperature: {light_to_temperature}")
-
-    i += 1
-    while isinstance(lines[i], (int, float)):
-        # Si el elemento es un número, haz algo con él
-        temperature_to_humidity.append(lines[i])
-        i += 1
-    print(f"temperature_to_humidity: {temperature_to_humidity}")
-
-    i += 1
-    while isinstance(lines[i], (int, float)):
-        # Si el elemento es un número, haz algo con él
-        humidity_to_location.append(lines[i])
-        i += 1
-        if i == len(lines): break
-    print(f"humidity_to_location: {humidity_to_location}")
+def parse_inputs(data: list[str]) -> list[list[str]]:
+    as_text = "\n".join(data)
+    return [line.split("\n") for line in as_text.split("\n\n")]
 
 
-def prepare_input():
-    sed_to_soil_3 = zip_longest(*[iter(sed_to_soil)] * 3, fillvalue=None)
-    for elem in sed_to_soil_3:
-        inicio1 = elem[0]
-        inicio2 = elem[1]
-        rango = elem[2]
+def part_one(data):
+    sections = parse_inputs(data)
+    seeds = {x: x for x in map(int, pattern.findall(sections[0][0]))}
+
+    for mapping in sections[1:]:
+        for seed, seed_value in seeds.items():
+            for _ in mapping[1:]:
+                dest, src, length = list(map(int, pattern.findall(_)))
+                if src <= seed_value < (src + length):
+                    seeds[seed] = dest + (seed_value - src)
+
+    print(seeds)
+
+
+def part_two(data):
+    sections = parse_inputs(data)
+    seeds_ranges = sections[0][0].split()[1:]
+    seeds = []
+
+    for i in range(0, len(seeds_ranges), 2):
+        start = int(seeds_ranges[i])
+        end = int(seeds_ranges[i]) + int(seeds_ranges[i + 1]) - 1
+        seeds.append([start, end])
+
+    for mapping in sections[1:]:
+
+        print(mapping)
+
+        for index, seed_range in enumerate(seeds):
+            start, end = seed_range
+
+            for _range in mapping[1:]:
+                destination, source, length = list(map(int, pattern.findall(_range)))
+
+                if source <= start and start < (source + length):
+                    seeds[index][0] = destination + (start - source)
+
+                    if end < source + length:
+                        seeds[index][1] = destination + (end - source)
+
+                    else:
+                        seeds[index][1] = destination + length - 1
+                        seeds.append([source + length, end])
+
+                elif source <= end and end < (source + length):
+                    seeds[index][1] = destination + (end - source)
+
+                    if start > source:
+                        seeds[index][0] = destination + (start - source)
+                    else:
+                        seeds[index][0] = destination
+                        seeds.append([start, source - 1])
+
+    print(min(min(seeds)))
 
 
 if __name__ == "__main__":
-    get_input_data_to_list()
-    prepare_input()
+    data = [line.rstrip() for line in open("input").readlines()]
+    # part_one(data)
+    part_two(data)
